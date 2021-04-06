10cb1c4fff383b797bfffc154132c745da7d9418
Arjun Singh <arjunhpes@gmail.com>
2021-04-06 12:46:21 +0530

    Create LoveLetter.java

---
+++ LoveLetter.java
@@ -0,0 +1,24 @@
+package com.Array;
+
+import java.util.*;
+
+public class LoveLetter {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		for (int i = 1; i <= a; i++) {
+			char[] string = in.next().toCharArray();
+			int si = 0, ei = string.length - 1, count = 0;
+			while (si < ei) {
+				if (string[si] > string[ei])
+					count += (string[si] - string[ei]);
+				else
+					count += (string[ei] - string[si]);
+				si++;
+				ei--;
+			}
+			System.out.println(count);
+		}
+	}
+}
